510d0c4daec39f61d5c62abaf1bc4046c8a350f8
Maximiliano Rios <52523737+mrios1999@users.noreply.github.com>
2019-12-11 16:36:18 -0500

    lab commit

--- prolog_lab_1.pl
+++ prolog_lab_1.pl
@@ -19,14 +19,23 @@ disjointRange([], _).
 disjointRange([Head|Tail], Z) :- disjointRange(Tail, Z), maxlist(Z, Max), Max > Head.
 
 %4. 
-%splitter(L, L1, L2).
-%merger(L1, L2, LN). 
-%mergesort(L, LS).
-
 splitter([], [], []).
 splitter([A], [A], []).
 splitter([First, Second|Tail], [First|L1], [Second|L2]) :- splitter(Tail, L1, L2).
 
-merger([], [], []).
-merger([A], [], [A]).
-merger([L1Head|L1Tail], [L2Head|L2Tail], Z) :- merger(L1Tail, L2Tail, MergedList), 
+merger(L, L, []).
+merger(L, [], L).
+merger([], L, L).
+merger([L1Head|L1Tail], [L2Head|L2Tail], [L1Head|Z]) :- L1Head =< L2Head, merger(L1Tail, [L2Head|L2Tail], Z).
+merger([L1Head|L1Tail], [L2Head|L2Tail], [L2Head|Z]) :- L2Head =< L1Head, merger([L1Head|L1Tail], L2Tail, Z).
+
+mergesort([], []).
+mergesort([A], [A]).
+mergesort([First, Second|Tail], LS) :-  splitter([First, Second|Tail], Split1, Split2), mergesort(Split1, Merge1), mergesort(Split2, Merge2), merger(Merge1, Merge2, LS).
+
+%5.
+%a. Insert a car by specifying the make and year.
+%b. Delete all cars of a certain make.
+%c. Check if a certain car exists in the database. 
+%d. List all cars of a specific year.
+
