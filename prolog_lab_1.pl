09f7f3ec1b1097c4fe20678886d0562465072cea
Maximiliano Rios <52523737+mrios1999@users.noreply.github.com>
2019-12-12 22:40:58 -0500

    lab commit

--- prolog_lab_1.pl
+++ prolog_lab_1.pl
@@ -23,7 +23,6 @@ splitter([], [], []).
 splitter([A], [A], []).
 splitter([First, Second|Tail], [First|L1], [Second|L2]) :- splitter(Tail, L1, L2).
 
-merger(L, L, []).
 merger(L, [], L).
 merger([], L, L).
 merger([L1Head|L1Tail], [L2Head|L2Tail], [L1Head|Z]) :- L1Head =< L2Head, merger(L1Tail, [L2Head|L2Tail], Z).
@@ -34,8 +33,19 @@ mergesort([A], [A]).
 mergesort([First, Second|Tail], LS) :-  splitter([First, Second|Tail], Split1, Split2), mergesort(Split1, Merge1), mergesort(Split2, Merge2), merger(Merge1, Merge2, LS).
 
 %5.
-%a. Insert a car by specifying the make and year.
-%b. Delete all cars of a certain make.
-%c. Check if a certain car exists in the database. 
-%d. List all cars of a specific year.
+%a. Insert a car by specifying the make and year. [X]
+%b. Delete all cars of a certain make. [X]
+%c. Check if a certain car exists in the database. [X] 
+%d. List all cars of a specific year. []
 
+addToInven(Make, Year, PreInventory, [car(Make, Year)|PreInventory]).
+
+deleteAllCars(M, [], []).
+deleteAllCars(Make, [car(Make,_)|PreInvTail], PostInventory) :- deleteAllCars(Make, PreInvTail, PostInventory).
+deleteAllCars(Make, [PreInvHead|PreInvTail], [PreInvHead|PostInventory]) :- deleteAllCars(Make, PreInvTail, PostInventory).
+
+findCar(Make, Year, Inventory) :- member(car(Make, Year), Inventory). 
+
+specificYear(Year, [], []).
+specificYear(Year, [car(Make, Year)|Inventory], CarsForYear) :- append([car(Make, Year)], CarsForYear1, CarsForYear), specificYear(Year, Inventory, CarsForYear1).
+specificYear(Year, [InvHead|Inventory], CarsForYear) :- specificYear(Year, Inventory, CarsForYear).
